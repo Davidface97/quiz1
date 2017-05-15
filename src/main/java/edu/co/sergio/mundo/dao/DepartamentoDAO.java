65dc089132cfdc95bc16a92e753e959d4941d006
Labing <Labing@XeonB101-PC-18.academia.loc>
2017-05-15 10:00:26 -0500

    Sirviendo

--- src/main/java/edu/co/sergio/mundo/dao/DepartamentoDAO.java
+++ src/main/java/edu/co/sergio/mundo/dao/DepartamentoDAO.java
@@ -16,24 +16,22 @@ import java.util.logging.Logger;
 /**
  * @author Isabel-Fabian
  * @since 12/08/2015
- * @version 2
- * Clase que permite la gestion de la tabla Depto en la base de datos.
- * 
- * CREATE TABLE Depto(
- *     id_depto integer,
- *     nom_depto varchar(40),
- *     PRIMARY KEY(id_depto)
- * );
+ * @version 2 Clase que permite la gestion de la tabla Depto en la base de
+ * datos.
+ *
+ * CREATE TABLE Depto( id_depto integer, nom_depto varchar(40), PRIMARY
+ * KEY(id_depto) );
  */
- 
-
 public class DepartamentoDAO implements IBaseDatos<Departamento> {
 
-	/**
-	 * Funcion que permite obtener una lista de los departamentos existentes en la base de datos
-	 * @return List<Departamento> Retorna la lista de Departamentos existentes en la base de datos
-	 */
-	/*public List<Departamento> findAll() {
+    /**
+     * Funcion que permite obtener una lista de los departamentos existentes en
+     * la base de datos
+     *
+     * @return List<Departamento> Retorna la lista de Departamentos existentes
+     * en la base de datos
+     */
+    /*public List<Departamento> findAll() {
 		List<Departamento> departamentos= null;
 	    String query = "SELECT * FROM Depto";
 	    Connection connection = null;
@@ -71,36 +69,38 @@ public class DepartamentoDAO implements IBaseDatos<Departamento> {
 	    
 	    return departamentos;
 	}*/
-
-	
-	/**
-	 * Funcion que permite realizar la insercion de un nuevo registro en la tabla Departamento
-	 * @param Departamento recibe un objeto de tipo Departamento 
-	 * @return boolean retorna true si la operacion de insercion es exitosa.
-	 */
-	public boolean insert(Obra_De_Arte t) {
-		boolean result=false;
-		Connection connection=null;
-            try {
-                connection = Conexion.getConnection();
-            } catch (URISyntaxException ex) {
-                Logger.getLogger(DepartamentoDAO.class.getName()).log(Level.SEVERE, null, ex);
-            }
-	    String query = "insert into ObraArte (usuario, nombre,descripcion,estilo,valor)"  + " values (?,?,?,?,?)";
-        PreparedStatement preparedStmt=null;
-	    try {
-			preparedStmt = connection.prepareStatement(query);
-			preparedStmt.setString (1, t.getUsuario());
-                        preparedStmt.setString (2, t.getNombre());
-                        preparedStmt.setString (3, t.getDescripcion());
-                        preparedStmt.setString (4, t.getEstilo());
-                        preparedStmt.setDouble(5, t.getValor());
-			result= preparedStmt.execute();
-	    } catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
+    /**
+     * Funcion que permite realizar la insercion de un nuevo registro en la
+     * tabla Departamento
+     *
+     * @param Departamento recibe un objeto de tipo Departamento
+     * @return boolean retorna true si la operacion de insercion es exitosa.
+     */
+    public boolean insert(Obra_De_Arte t) {
+        boolean result;
+        Connection connection = null;
+        try {
+            connection = Conexion.getConnection();
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(DepartamentoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String query = "insert into ObraArte (usuario, nombre,descripcion,estilo,valor)" + " values (?,?,?,?,?)";
+        PreparedStatement preparedStmt = null;
+        try {
+            preparedStmt = connection.prepareStatement(query);
+            preparedStmt.setString(1, t.getUsuario());
+            preparedStmt.setString(2, t.getNombre());
+            preparedStmt.setString(3, t.getDescripcion());
+            preparedStmt.setString(4, t.getEstilo());
+            preparedStmt.setDouble(5, t.getValor());
+            result = preparedStmt.execute();
+            result = true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            result = false;
+        }
+        return result;
+    }
 
     @Override
     public List<Departamento> findAll() {
@@ -122,12 +122,14 @@ public class DepartamentoDAO implements IBaseDatos<Departamento> {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-	/**
-	 * Funcion que permite realizar la actualizacion de un nuevo registro en la tabla Departamento
-	 * @param Departamento recibe un objeto de tipo Departamento 
-	 * @return boolean retorna true si la operacion de actualizacion es exitosa.
-	 */
-	/*public boolean update(Departamento t) {
+    /**
+     * Funcion que permite realizar la actualizacion de un nuevo registro en la
+     * tabla Departamento
+     *
+     * @param Departamento recibe un objeto de tipo Departamento
+     * @return boolean retorna true si la operacion de actualizacion es exitosa.
+     */
+    /*public boolean update(Departamento t) {
 		boolean result=false;
 		Connection connection= null;
             try {
@@ -151,13 +153,14 @@ public class DepartamentoDAO implements IBaseDatos<Departamento> {
 			
 		return result;
 	}*/
-
-	/**
-	 * Funcion que permite realizar la eliminario de registro en la tabla Departamento
-	 * @param Departamento recibe un objeto de tipo Departamento 
-	 * @return boolean retorna true si la operacion de borrado es exitosa.
-	 */
-	/*public boolean delete(Departamento t) {
+    /**
+     * Funcion que permite realizar la eliminario de registro en la tabla
+     * Departamento
+     *
+     * @param Departamento recibe un objeto de tipo Departamento
+     * @return boolean retorna true si la operacion de borrado es exitosa.
+     */
+    /*public boolean delete(Departamento t) {
 	   boolean result=false;
 	   Connection connection = null;
             try {
